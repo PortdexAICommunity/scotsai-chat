bc30979e3457f39ab76a67dfaaaa23f954c30a5b
Dipesh <99866329+pilladipesh33@users.noreply.github.com>
2025-06-18 13:09:03 +0530

    feat:connected ai model with app

--- lib/ai/entitlements.ts
+++ lib/ai/entitlements.ts
@@ -1,29 +1,41 @@
-import type { UserType } from '@/app/(auth)/auth';
-import type { ChatModel } from './models';
+import type { UserType } from "@/app/(auth)/auth";
+import type { ChatModel } from "./models";
 
 interface Entitlements {
-  maxMessagesPerDay: number;
-  availableChatModelIds: Array<ChatModel['id']>;
+	maxMessagesPerDay: number;
+	availableChatModelIds: Array<ChatModel["id"]>;
 }
 
 export const entitlementsByUserType: Record<UserType, Entitlements> = {
-  /*
-   * For users without an account
-   */
-  guest: {
-    maxMessagesPerDay: 20,
-    availableChatModelIds: ['chat-model', 'chat-model-reasoning'],
-  },
+	/*
+	 * For users without an account
+	 */
+	guest: {
+		maxMessagesPerDay: 20,
+		availableChatModelIds: [
+			"chat-model",
+			"chat-model-reasoning",
+			"chat-model-search",
+			"chat-model-news",
+			"chat-model-find",
+		],
+	},
 
-  /*
-   * For users with an account
-   */
-  regular: {
-    maxMessagesPerDay: 100,
-    availableChatModelIds: ['chat-model', 'chat-model-reasoning'],
-  },
+	/*
+	 * For users with an account
+	 */
+	regular: {
+		maxMessagesPerDay: 100,
+		availableChatModelIds: [
+			"chat-model",
+			"chat-model-reasoning",
+			"chat-model-search",
+			"chat-model-news",
+			"chat-model-find",
+		],
+	},
 
-  /*
-   * TODO: For users with an account and a paid membership
-   */
+	/*
+	 * TODO: For users with an account and a paid membership
+	 */
 };
